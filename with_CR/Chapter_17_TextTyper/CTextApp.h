95f2692cc068a81ad4cf12fddbe8e3156d2c97fd
greenonline <greenonline@users.noreply.github.com>
2024-07-03 21:00:56 +0100

    Add files via upload

---
+++ with_CR/Chapter_17_TextTyper/CTextApp.h
@@ -0,0 +1,1 @@
+/* Chapter 17 - TextTyper *//* CTextApp.h */#pragma once#include <CApplication.h>class CTextApp: public CApplication {public:    CTextApp();    virtual void CreateDocument();    virtual void OpenDocument(SFReply *macReply);    virtual void SetUpFileParameters();};
---
+++ with_CR/Chapter_17_TextTyper/Text.cp
@@ -0,0 +1,1 @@
+/* Chapter 17 - TextTyper *//* Text.cp */#include "CTextApp.h"void main(){    CTextApp *app;    app = new CTextApp;    app->Run();    app->Exit();}
