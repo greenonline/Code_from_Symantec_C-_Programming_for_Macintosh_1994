831f3a13884968bbe47e037f2fa2f0a5a2fc84b0
greenonline <greenonline@users.noreply.github.com>
2024-07-04 11:02:45 +0100

    Add files via upload

---
+++ with_CR/Chapter_20_C++Calc/CCalculatorApp.h
@@ -0,0 +1,1 @@
+/* Chapter 20 - C++Calc *//* CCalculatorApp.h *//****** CCalculatorApp.h*** *****/#pragma once#include "CAboutBoxApplication.h"class CCalculatorApp: public CAboutBoxApplication {public:    CCalculatorApp();    void StartUpAction(short numPreloads);};
---
+++ with_CR/Chapter_20_C++Calc/CQuitApplicationChore.h
@@ -0,0 +1,1 @@
+/* Chapter 20 - C++Calc *//* CQuitApplicationChore.h */#pragma once #include <CChore.h>class CQuitApplicationChore: public CChore { public:    virtual void Perform(long *maxSleep);};
